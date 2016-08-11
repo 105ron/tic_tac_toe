99f8274355b5e3ed8e1518e55e9a8333d048476c
rhys <rbrooker105@gmail.com>
2016-08-11 20:25:44 +1000

    Add any_empty and all_empty to core extensions and spec

--- spec/core_extensions_spec.rb
+++ spec/core_extensions_spec.rb
@@ -27,4 +27,30 @@ describe Array do
       expect([].all_same?).to be_truthy
     end
   end
+  context "#any_empty?" do
+    it "returns true if any elements of the Array are the empty" do
+      expect(["A", "A", ""].any_empty?).to be_truthy
+    end
+ 
+    it "returns false if all of the Array elements are assigned" do
+      expect(["A", 1, "Cat", "5.7"].any_empty?).to be_falsey
+    end
+ 
+    it "returns false for an empty Array" do
+      expect([].any_empty?).to be_falsey
+    end
+  end
+  context "#all_empty?" do
+    it "returns true if all elements of the Array are the empty" do
+      expect(["", "", ""].all_empty?).to be_truthy
+    end
+ 
+    it "returns false if any of the Array elements are assigned" do
+      expect(["", 1, "", ""].all_empty?).to be_falsey
+    end
+ 
+    it "returns true for an empty Array" do
+      expect([].all_empty?).to be_truthy
+    end
+  end
 end
